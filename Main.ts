9059f69421bc9641aa1126e79bc342ec2eab9760
Usama Muzammil <160235187+UsamaIT786@users.noreply.github.com>
2024-05-03 08:04:33 -0700

    Add files via upload

---
+++ Main.ts
@@ -0,0 +1,9 @@
+//Self Check Quiz 
+
+let answer: string = "Governer IT Student"
+
+if(answer === "Governer IT Student"){
+console.log("Your Enrolled In This IT Cource")
+}else{
+    console.log("Your Not Selected")
+}
---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+//Self Check Quiz 
+let answer = "Governer IT Student";
+if (answer === "Governer IT Student") {
+    console.log("Your Enrolled In This IT Cource");
+}
+else {
+    console.log("Your Not Selected");
+}
